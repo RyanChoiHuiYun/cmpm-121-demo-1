ab13504c7967ab586ea812edcd1673205061b376
Str4wbeary <xcai30@ucsc.edu>
2023-10-07 23:37:44 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,13 @@ import "./style.css";
 const app: HTMLDivElement = document.querySelector("#app")!;
 
 const gameName = "Ryan's game";
+const buttonName = "&#128059;";
 
 document.title = gameName;
 
 const header = document.createElement("h1");
+const button = document.createElement("button");
 header.innerHTML = gameName;
+button.innerHTML = buttonName;
 app.append(header);
+app.append(button);
