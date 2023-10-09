f1f7d1927982a79a22e699975e87f924469122be
Str4wbeary <xcai30@ucsc.edu>
2023-10-08 21:26:28 -0700

    step 3 complete

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,38 @@ const app: HTMLDivElement = document.querySelector("#app")!;
 
 const gameName = "Ryan's game";
 const buttonName = "&#128059;";
+let bearCounter: number = 0;
 
 document.title = gameName;
 
-const header = document.createElement("h1");
-const button = document.createElement("button");
+const header: HTMLHeadingElement = document.createElement("h1");
 header.innerHTML = gameName;
-button.innerHTML = buttonName;
 app.append(header);
-app.append(button);
+
+const bearButton: HTMLButtonElement = document.createElement("button");
+bearButton.className = "bearButton";
+bearButton.type = "button";
+bearButton.innerHTML = buttonName;
+
+app.append(bearButton);
+
+const bearCounting: HTMLDivElement = document.createElement("div");
+bearCounting.textContent = getBearCount();
+
+bearButton.addEventListener("click", onClicked);
+app.append(bearButton);
+app.append(bearCounting);
+
+function onClicked() {
+  bearCounter += 1;
+  bearCounting.textContent = getBearCount();
+}
+
+function getBearCount() {
+  return `${bearCounter} bears`;
+}
+
+setInterval(function () {
+  bearCounter += 1;
+  bearCounting.textContent = getBearCount();
+}, 4000);
