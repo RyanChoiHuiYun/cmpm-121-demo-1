42a1c36ec8598eefcbef284226582e4e0d35103c
Ryan Choi <xcai30@ucsc.edu>
2023-10-10 00:23:58 -0700

    step 5 sorta complete (?)

--- src/main.ts
+++ src/main.ts
@@ -2,52 +2,72 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "Ryan's game";
+const gameName = "Catch'em Bears!";
 const buttonName = "&#128059;";
 let bearCounter: number = 0;
 
 document.title = gameName;
 
+// h1
 const header: HTMLHeadingElement = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
 
+// increment button
 const bearButton: HTMLButtonElement = document.createElement("button");
 bearButton.className = "bearButton";
 bearButton.type = "button";
 bearButton.innerHTML = buttonName;
-
-const bearCounting: HTMLDivElement = document.createElement("div");
-bearCounting.textContent = getBearCount();
-
-bearButton.addEventListener("click", onClicked);
+bearButton.addEventListener("click", bearOnClicked);
 app.append(bearButton);
+
+// number display
+const bearCounting: HTMLDivElement = document.createElement("div");
+bearCounting.textContent = `${getBearCount()} bears`;
 app.append(bearCounting);
 
-function onClicked() {
+// skill button
+const skillButton: HTMLButtonElement = document.createElement("button");
+skillButton.className = "skillButton";
+skillButton.type = "button";
+skillButton.textContent = "Cost 10: bear trap (honey)";
+skillButton.addEventListener("click", skillOnClicked);
+app.append(skillButton);
+
+if (bearCounter < 10) {
+  skillButton.disabled = true;
+}
+
+window.requestAnimationFrame(frameFunction);
+
+function bearOnClicked() {
   bearCounter += 1;
-  bearCounting.textContent = getBearCount();
+  bearCounting.textContent = `${getBearCount()} bears`;
+}
+
+function skillOnClicked() {
+  itemBought = true;
+  bearCounter -= 10;
+  bearCounting.textContent = `${getBearCount()} bears`;
 }
 
 function getBearCount() {
-  return `${bearCounter} bears`;
+  return bearCounter;
 }
 
-//setInterval(function () {
-//  bearCounter += 1;
-//  bearCounting.textContent = getBearCount();
-//}, 4000);
+let then: number = performance.now();
+let timer: number = 0;
+let itemBought: boolean = false;
 
-let before: number = Date.now();
-let now: number;
-let fps: number = 0;
+function frameFunction() {
+  timer += performance.now() - then;
+  if (timer >= 1000 && itemBought == true) {
+    bearCounter += 1;
+    bearCounting.textContent = `${getBearCount()} bears`;
+    timer = 0;
+  }
+  skillButton.disabled = bearCounter < 10;
 
-requestAnimationFrame(function loop() {
-  now = Date.now();
-  fps = Math.round(1000 / (now - before));
-  before = now;
-  requestAnimationFrame(loop);
-  bearCounter += 1 / fps;
-  bearCounting.textContent = getBearCount();
-  console.log("fps", fps);
-});
+  then = performance.now();
+  window.requestAnimationFrame(frameFunction);
+}
